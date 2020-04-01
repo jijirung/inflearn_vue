1093cd1587c84710c3b3004564fd29801befe4fe
jungjihyung <gw031763@mcloudoc.com>
2020-04-01 18:12:57 +0900

    처음생성한 vue-news

---
+++ src/views/NewsView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ users }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data(){
+    return{
+      users: []
+    }
+  },
+  created() {
+    var vm = this;
+    axios.get('	https://api.hnpwa.com/v0/news/1.json')
+    .then(response =>{
+      vm.users = response.data;
+    } )
+    .catch(error=> console.log(error))
+  }
+
+}
+</script>
+
+<style>
+
+</style>
