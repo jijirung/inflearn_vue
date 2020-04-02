3ea7e52e26d0b89f02831cc1a726b8826b0d120c
jungjihyung <gw031763@mcloudoc.com>
2020-04-02 15:06:02 +0900

    five commit

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes/index.js';
-<<<<<<< HEAD
 import { store } from './store/index.js'
-=======
->>>>>>> 1093cd1587c84710c3b3004564fd29801befe4fe
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   router,
-<<<<<<< HEAD
   store
 }).$mount("#app");
-=======
-}).$mount('#app')
->>>>>>> 1093cd1587c84710c3b3004564fd29801befe4fe
